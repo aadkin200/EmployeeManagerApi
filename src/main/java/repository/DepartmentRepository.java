336f19b4c65fbf229a7e146d2a64c2056e8065ab
aadkin200 <alex.t.adkins@gmail.com>
2026-03-30 16:54:01 -0400

    added some repository functionality

---
+++ src/main/java/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apsoftware.employeemanagerapi.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, String>{
+	Optional<Department> findByDeptName(String deptName);
+}
---
+++ src/main/java/repository/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apsoftware.employeemanagerapi.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	List<Employee> findByLastName(String lastName);
+
+    List<Employee> findByFirstNameAndLastName(String firstName, String lastName);
+
+    List<Employee> findByHireDateAfter(LocalDate date);
+}
